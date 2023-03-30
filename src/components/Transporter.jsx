cd6f284abda3ccca19b4609e33d816583e8a3589
Barakat Bolaji <barakatbolaji65@gmail.com>
2023-03-30 23:11:35 +0100

    - implementing the transporter page

--- src/components/Transporter.jsx
+++ src/components/Transporter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Transporter.module.css";
-
+import pilot from "../asset/pilot.jpg";
 import trans from "../asset/trans.jpg";
 import driver from "../asset/driver.jpg";
 
@@ -8,12 +8,12 @@ export default function Transporter() {
   return (
     <section className={style.transport}>
       <div className="container">
-        <div>
+        <div className={style.header}>
           <span className="highlighted">The Transporters</span>
           <h3>Meet Expert Team</h3>
         </div>
         <div className="images">
-          <img src={} alt="photo" />
+          <img src={pilot} alt="photo" />
           <img src={trans} alt="photo" />
           <img src={driver} alt="photo" />
         </div>
